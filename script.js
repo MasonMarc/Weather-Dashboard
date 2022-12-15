ba6a08bd62bfc61c932153a23a040dddc0ee4092
MasonMarc <masonmarcantel@gmail.com>
2022-12-14 19:36:37 -0500

    made js work in less lines of code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var buttonEl = $('#Btn')
 $(buttonEl).click(function (lat, lon) {
   event.preventDefault();
   var cityName =  $('#cityName').val();
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName+'&appid=9d3d5cb93416cbf527eb8fd87e269c23'
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ cityName+'&appid=9d3d5cb93416cbf527eb8fd87e269c23&units=imperial'
     fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
@@ -24,9 +24,18 @@ $(buttonEl).click(function (lat, lon) {
 
 
   var displayWeather = function (data) {  
-    $('#mainHead').text(data.name);
-    $('#mainTitle').text(data.main.temp);
-    $('#mainBody').text(data.weather[0].description);
-  
+    $('#mainHead').text(data.city.name + ', ' + data.city.country + ' Today');
+    $('#mainTitle').text(data.list[0].main.temp + " Fahrenheit");
+    $('#mainBody').text(data.list[0].weather[0].description);
 
+
+    // smaLL cards -------------
+    for(var i=1;i<6;i++){
+
+    $('#dayHead'+i).text(data.city.name + ', ' + data.city.country);
+    $('#dayTitle'+i).text(data.list[i].main.temp + " Fahrenheit");
+    $('#dayBody'+i).text(data.list[i].weather[0].description);
+
+    }
+    // Need to add date, icon, humidity, and wind speed
   }
